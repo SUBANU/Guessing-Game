201709dedaa1cb410d506a95c7c7b2eef9c8d21b
SUBANU <subanukjy@gmail.com>
2021-06-08 21:22:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const randomNumber=Math.floor(Math.random()*9+1);
+const userGuess=document.querySelector("#input");
+const resultDiv=document.querySelector(".result");
+const historyDiv=document.querySelector(".history");
+
+
+historyDiv.innerHTML="";
+
+
+function game() {
+
+    let result;
+    let guess=userGuess.value;
+    
+    let chances=5;
+    let iswin=false;
+
+    if(guess>10|| guess<1) 
+    {
+        result="Enter a number between 1 and 10"; 
+    }
+    else if(chances || !iswin){
+        let guess=userGuess.value;
+        if(randomNumber==guess)
+        {
+            result="you won!";
+            iswin=true;
+        }
+        else if(randomNumber<guess) 
+        {
+            if(chances==1){
+                result=`Game Over! <br> The Number is ${randomNumber}`;
+            }
+            else{
+                result="your guess is HIGH!";
+            }
+        }
+        else {
+            if(chances==1) {
+                result=`Game Over! <br> The Number is${randomNumber}`;
+        }else{
+                result="you guess is LOW!";
+        
+        }
+        
+    }
+    chances--;
+    console.log(chances)
+
+    
+    }
+
+    historyDiv.innerHTML+=`<h4>You guessed ${guess}</h4>`;
+    return resultDiv.innerHTML=`<h4>${result}</h4>`;
+
+
+}
+function reload()
+{
+    if(!chances||iswin){
+        location.reload();
+    }
+}
