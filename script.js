3fc92a25d62322438e170e355be7ab1bb94fc2ad
SUBANU <subanukjy@gmail.com>
2021-06-09 16:28:09 +0530

    second commit

--- script.js
+++ script.js
@@ -1,63 +1,51 @@
-const randomNumber=Math.floor(Math.random()*9+1);
-const userGuess=document.querySelector("#input");
-const resultDiv=document.querySelector(".result");
-const historyDiv=document.querySelector(".history");
+const randomNumber = Math.floor(Math.random()*99+1);
+const userGuess = document.querySelector("#input");
+const resultDiv = document.querySelector(".result");
+const historyDiv = document.querySelector(".history");
 
 
-historyDiv.innerHTML="";
 
+historyDiv.innerHTML = "";
+
+let chances = 5;
+
+let isWin = false;
 
 function game() {
-
     let result;
-    let guess=userGuess.value;
-    
-    let chances=5;
-    let iswin=false;
-
-    if(guess>10|| guess<1) 
-    {
-        result="Enter a number between 1 and 10"; 
-    }
-    else if(chances || !iswin){
-        let guess=userGuess.value;
-        if(randomNumber==guess)
-        {
-            result="you won!";
-            iswin=true;
-        }
-        else if(randomNumber<guess) 
-        {
-            if(chances==1){
-                result=`Game Over! <br> The Number is ${randomNumber}`;
+    let guess = userGuess.value;
+    if (guess > 100 || guess < 1) {
+        result = "Guess a number between 1 and 100";
+        resultDiv.innerHTML = `<h4>${result}</h4>`;
+    }else if (chances && !isWin) {
+        let guess = userGuess.value;
+        if (randomNumber == guess) {
+            result = "You won!";
+            isWin = true;
+        } else if (randomNumber < guess) {
+            if (chances == 1) {
+                result = `Game Over!<br>The Number is ${randomNumber}`;
+            }else {
+                result = "You too high";
             }
-            else{
-                result="your guess is HIGH!";
+        }else {
+            if (chances == 1) {
+                result = `Game Over!<br>The Number is ${randomNumber}`;
+            }else {
+                result = "You too low!";
             }
         }
-        else {
-            if(chances==1) {
-                result=`Game Over! <br> The Number is${randomNumber}`;
-        }else{
-                result="you guess is LOW!";
-        
-        }
-        
+        chances--;
+        historyDiv.innerHTML += `<h4>You Guessed ${guess}</h4>`;
+        return resultDiv.innerHTML = `<h4>${result}</h4>`;
     }
-    chances--;
-    console.log(chances)
-
     
-    }
-
-    historyDiv.innerHTML+=`<h4>You guessed ${guess}</h4>`;
-    return resultDiv.innerHTML=`<h4>${result}</h4>`;
-
-
 }
-function reload()
-{
-    if(!chances||iswin){
+
+
+function reload() {
+    if (!chances || isWin) {
         location.reload();
     }
+    
 }
